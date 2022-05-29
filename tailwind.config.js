a6742387ed8989ec49035b2864b0bdb6ea31a5c6
andymartin-work <106444848+andymartin-work@users.noreply.github.com>
2022-05-28 22:34:43 -0700

    adding tailwind for easier updates
    tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    extend: {
+      colors:{
+        myGreentail:'#00a797'
+  
+      },
+    },
+  },
+  plugins: [require("daisyui")],
+}
